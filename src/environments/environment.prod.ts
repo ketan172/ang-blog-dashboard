e64088769f9b02f1743bfaab943910ef8911a888
ketan172 <ketankolte17@gmail.com>
2023-03-12 01:21:33 +0530

    ang-blog-dashboard

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyAUxtIDvDMwQO3Z2gFBatFmSVZDISjdEAI",
+        authDomain: "ang-blog-4e656.firebaseapp.com",
+        projectId: "ang-blog-4e656",
+        storageBucket: "ang-blog-4e656.appspot.com",
+        messagingSenderId: "1087722842416",
+        appId: "1:1087722842416:web:d011d3f8564d2c6483c0f5"
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+export const environment = {
+    production: false,
+   
+ 
+}
+
+// // Use this to initialize the firebase App
+// const firebaseApp = firebase.initializeApp(firebase);
+
+// // Use these for db & auth
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+// export { auth, db };
+
